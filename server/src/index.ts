4aef6834e6702298fac6e960868dfae5697e498b
Timo Wenz <2025014@stud.hs-mannheim.de>
2024-02-22 21:46:01 +0100

    feat: client and server listening on port 5000 and 3000. Architecture.drawio adjusted. README.md added.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-// Index file for server
+import express from "express";
+import { json } from "body-parser";
+
+const app = express();
+
+app.use(json());
+
+app.get("/", (req, res) => {
+  try {
+    return res.json("Hello World");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
