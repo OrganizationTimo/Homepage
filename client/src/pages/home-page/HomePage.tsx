09462b1cec6b697fe9e91b548073d5df3ed1181e
Timo Wenz <2025014@stud.hs-mannheim.de>
2024-03-01 16:55:35 +0100

    feat: changed package manager to pnpm. Homepage with Hero component added.

--- client/src/pages/home-page/HomePage.tsx
+++ client/src/pages/home-page/HomePage.tsx
@@ -1,7 +1,5 @@
+import Hero from "./components/Hero";
+
 export default function HomePage() {
-  return (
-    <>
-      <div className="w-full h-screen"></div>
-    </>
-  );
+  return <div className="w-full h-screen">Hello worlc</div>;
 }
