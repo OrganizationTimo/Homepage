f80ffed5bacce7f53dffbc1a0bc4eaf93b3a2664
Timo Wenz <2025014@stud.hs-mannheim.de>
2024-03-02 16:48:00 +0100

    Update database configuration to use environment variables

--- server/src/configs/db.config.ts
+++ server/src/configs/db.config.ts
@@ -1,9 +1,9 @@
 import { Client } from "pg";
 
 export const client = new Client({
-  host: "localhost",
-  port: 5432,
-  database: "timowenz",
-  user: "admin",
-  password: "tygCaj-cuqkuw-8wypvi",
+  host: process.env.HOST || "localhost",
+  port: process.env.PORT ? parseInt(process.env.PORT) : 5432,
+  database: process.env.NAME || "postgres",
+  user: process.env.USER || "postgres",
+  password: process.env.PASSWORD || "postgres",
 });
